9153c8ea2485a756b7a63371a3802ea1832554e8
Tolulope Olofinjana <tolujana@gmail.com>
2025-05-26 12:49:56 +0100

    fixed issue with module where acessibility seting not gotton

--- android/app/src/main/java/com/copilot3/ServiceHelperModule.kt
+++ android/app/src/main/java/com/copilot3/ServiceHelperModule.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import android.provider.Settings
 import com.facebook.react.bridge.*
 import com.facebook.react.module.annotations.ReactModule
+import android.content.ComponentName
+import android.content.Context
+import android.view.accessibility.AccessibilityManager
+import android.accessibilityservice.AccessibilityServiceInfo
+import com.facebook.react.bridge.Promise
 
 @ReactModule(name = ServiceHelperModule.NAME)
 class ServiceHelperModule(private val reactContext: ReactApplicationContext) :
@@ -25,7 +30,7 @@ class ServiceHelperModule(private val reactContext: ReactApplicationContext) :
     }
 
     @ReactMethod
-    fun isAccessibilityServiceEnabled(serviceId: String, promise: Promise) {
+    fun isAccessibilityServiceEnabled2(serviceId: String, promise: Promise) {
         try {
             val accessibilityEnabled = Settings.Secure.getInt(
                 reactContext.contentResolver,
@@ -55,4 +60,37 @@ class ServiceHelperModule(private val reactContext: ReactApplicationContext) :
             promise.reject("ACCESSIBILITY_CHECK_FAILED", e)
         }
     }
+     @ReactMethod
+    fun isAccessibilityServiceEnabled(serviceId: String, promise: Promise) {
+    try {
+        val am = reactContext.getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager
+
+        val accessibilityEnabled = android.provider.Settings.Secure.getInt(
+            reactContext.contentResolver,
+            android.provider.Settings.Secure.ACCESSIBILITY_ENABLED,
+            0
+        ) == 1
+
+        if (!accessibilityEnabled) {
+            promise.resolve(false)
+            return
+        }
+
+        val enabledServices = am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK)
+
+        for (serviceInfo in enabledServices) {
+            if (serviceInfo.id.equals(serviceId, ignoreCase = true)) {
+                promise.resolve(true)
+                return
+            }
+        }
+
+        promise.resolve(false)
+    } catch (e: Exception) {
+        promise.reject("ACCESSIBILITY_CHECK_FAILED", e)
+    }
+}
+
+
+
 }
