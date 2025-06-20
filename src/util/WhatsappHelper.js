97e4323684c9ddaf66a825ed64a1e36abbbac826
Tolulope Olofinjana <tolujana@gmail.com>
2025-06-20 18:34:14 +0100

    added more functions

--- src/util/WhatsappHelper.js
+++ src/util/WhatsappHelper.js
@@ -1,10 +1,35 @@
-import {NativeModules} from 'react-native';
-const {WhatsappServiceModule} = NativeModules;
+import {NativeModules, Platform} from 'react-native';
+const {AppServiceModule, AccessibilityHelper} = NativeModules;
 
 export const launchWhatsappMessage = (contacts, whatsappType) => {
   console.log('this is contact', contacts);
-  WhatsappServiceModule.startSendingMessages(
+  AppServiceModule.startSendingMessages(
     JSON.stringify(contacts),
-    whatsappType, //
+    whatsappType,
+    //
   );
+};
+
+export const checkOverlayPermission = async () => {
+  if (Platform.OS !== 'android') return true;
+  return await AccessibilityHelper.isOverlayPermissionGranted();
+};
+
+export const openOverlaySettings = () => {
+  if (Platform.OS === 'android') {
+    AccessibilityHelper.openOverlaySettings();
+  }
+};
+export const checkAccessibilityPermission = async () => {
+  // Note the format: package name + '/' + full service class name with leading dot
+  const serviceId = 'com.copilot3/.AppAccessibilityService';
+  try {
+    const enabled = await AccessibilityHelper.isAccessibilityServiceEnabled(
+      serviceId,
+    );
+    return enabled;
+  } catch (e) {
+    console.warn('Failed to check accessibility permission', e);
+    return false;
+  }
 };
